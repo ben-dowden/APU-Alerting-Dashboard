fb96abccb584db9d0497b46e3b1301d22255e3ec
Ben Dowden <143870433+ben-dowden@users.noreply.github.com>
2026-05-22 18:46:34 +1000

    test-add-next-route-smoke-tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,16 @@
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  esbuild: {
+    jsx: "automatic",
+    jsxImportSource: "react",
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL(".", import.meta.url)),
+    },
+  },
   test: {
     environment: "jsdom",
     globals: true,
