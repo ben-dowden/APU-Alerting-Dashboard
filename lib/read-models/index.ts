f2d08dfa66207f843611f4ca6effd1cfd776d838
Ben Dowden <143870433+ben-dowden@users.noreply.github.com>
2026-05-23 14:09:45 +1000

    feat-add-hq-report-read-model

--- lib/read-models/index.ts
+++ lib/read-models/index.ts
@@ -3,4 +3,5 @@ export * from "./benchmark-panel";
 export * from "./current-board";
 export * from "./daily-scorecard";
 export * from "./data-quality";
+export * from "./hq-report";
 export * from "./reason-tagged-burn";
